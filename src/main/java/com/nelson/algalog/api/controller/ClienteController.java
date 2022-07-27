96cb48a29c43e11ee8946478e5c4096916861a82
Nelson <punkpunkada@gmail.com>
2022-07-26 22:15:36 -0300

    Get route

---
+++ src/main/java/com/nelson/algalog/api/controller/ClienteController.java
@@ -0,0 +1,30 @@
+package com.nelson.algalog.api.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nelson.algalog.domain.model.Cliente;
+
+@RestController
+public class ClienteController {
+	@GetMapping("/clientes")
+	public List<Cliente> listar() {
+		
+		var cliente = new Cliente();
+		cliente.setId(1);
+		cliente.setNome("Nelson");
+		cliente.setEmail("exemplo@exemplar.com");
+		cliente.setTelefone("5511953572284");
+		
+		Cliente cliente2 = new Cliente();
+		cliente2.setId(2);
+		cliente2.setNome("Noeme");
+		cliente2.setEmail("exemplar@exemplo.com");
+		cliente2.setTelefone("5511958791414");
+		
+		return Arrays.asList(cliente,cliente2) ;
+	}
+}
