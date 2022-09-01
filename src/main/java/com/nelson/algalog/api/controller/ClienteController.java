058ad61e05b7c16f20fa08aeb9edcebd6fbd636b
Nelson <punkpunkada@gmail.com>
2022-09-01 18:46:22 -0300

    Usando Jakarta e anotações SpringBoot

--- src/main/java/com/nelson/algalog/api/controller/ClienteController.java
+++ src/main/java/com/nelson/algalog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.nelson.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,21 +12,14 @@ import com.nelson.algalog.domain.model.Cliente;
 
 @RestController 
 public class ClienteController {
+	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		
-		Cliente cliente = new Cliente();
-		cliente.setId(1);
-		cliente.setNome("Nelson");
-		cliente.setEmail("exemplo@exemplar.com");
-		cliente.setTelefone("5511953572284");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2);
-		cliente2.setNome("Noeme");
-		cliente2.setEmail("exemplar@exemplo.com");
-		cliente2.setTelefone("5511958791414");
-		
-		return Arrays.asList(cliente,cliente2) ;
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 }
